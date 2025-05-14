be4bf6a04a5c0ce1e13154de4fd2038712c507ed
Mezanur Rahman <Mezanurrahman53@gmail.com>
2025-05-14 17:40:09 -0400

    master

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,5 +5,5 @@ export default function Home() {
       <h2 className="text-2xl font-semibold text-gray-700 mb-1">Qureshi, Anique</h2>
       <h3 className="text-xl text-gray-600">Spring 2025</h3>   
     </>
-  )
+  );
 }
