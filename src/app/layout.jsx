f13a8f3d6475af926b2e005c286c4dd4164cc2f4
Mezanur Rahman <Mezanurrahman53@gmail.com>
2025-05-14 20:52:57 -0400

    title

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Qureshi, Anique Final",
+  title: "Rahman, Mezanur Final",
 };
 
 export default function RootLayout({ children }) {
