2a50daffa6542a75fc6b2c34d494f62cf71f6ccc
Mezanur Rahman <Mezanurrahman53@gmail.com>
2025-05-23 15:19:47 -0400

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: true, // Add if TS errors
+  },
+  output: 'standalone', // Recommended for Vercel
 };
 
 export default nextConfig;
